bcbe8086fce80d6f202435334fc5698a9c546be0
aleksandrvolostnov <99894949+aleksandrvolostnov@users.noreply.github.com>
2023-12-06 23:29:40 +0300

    Add files via upload

---
+++ UserRepository.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use DateTime;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\TaskStatus;
+use Doctrine\ORM\AbstractQuery;
+use Exception;
+
+/**
+ * @method User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User[]    findAll()
+ * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    public function getSummary($days)
+    {
+        return $this
+            ->createQueryBuilder('u')
+            ->select('u.id, u.name, count(t.id) cnt')
+            ->innerJoin('u.tasks', 't')
+            ->where('t.deadline BETWEEN :start AND :now')
+            ->andWhere('t.status = :status')
+            ->setParameter('start', (new DateTime())->modify(-$days . ' days'))
+            ->setParameter('now', new DateTime())
+            ->setParameter('status', TaskStatus::STATUS_DONE)
+            ->setMaxResults(10)
+            ->groupBy('u.id')
+            ->orderBy('cnt', 'desc')
+            ->getQuery()
+            ->getResult(AbstractQuery::HYDRATE_ARRAY);
+    }
+
+    public function getTimeSummary($days)
+    {
+        return $this
+            ->createQueryBuilder('u')
+            ->select('u.id, u.name, sum(t.timeSpent) / 60 cnt')
+            ->innerJoin('u.tasks', 't')
+            ->where('t.deadline BETWEEN :start AND :now')
+            ->andWhere('t.status = :status')
+            ->setParameter('start', (new DateTime())->modify(-$days . ' days'))
+            ->setParameter('now', new DateTime())
+            ->setParameter('status', TaskStatus::STATUS_DONE)
+            ->setMaxResults(20)
+            ->groupBy('u.id')
+            ->orderBy('cnt', 'desc')
+            ->getQuery()
+            ->getResult(AbstractQuery::HYDRATE_ARRAY);
+    }
+}
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+composer install
+
+#php bin/console lexik:jwt:generate-keypair
+php bin/console make:entity App --regenerate -n
+php bin/console doctrine:database:drop --if-exists --force
+php bin/console doctrine:database:create
+php bin/console doctrine:schema:update --force
+php bin/console doctrine:schema:validate
+php bin/console doctrine:migration:migrate --no-interaction
+php bin/console doctrine:fixtures:load --append
